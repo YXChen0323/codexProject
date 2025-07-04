82a3252a1616fad5e3973a7a8b8800c569d3d11c
YXChen0323 <yxchen0323@gmail.com>
2025-07-04 09:30:26 +0800

    Update Tailwind CSS configuration and binary files

--- MCP_119/frontend/home/tailwind.config.js
+++ MCP_119/frontend/home/tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",  // 告訴 Tailwind 去掃描這些檔案.
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
